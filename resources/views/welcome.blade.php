0cef744c9ae6a95f9ae26f3d9e87c8175c6130cc
liuhui <liuhui@github.com>
2019-03-04 22:29:17 +0800

    use env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel {{ config('app.env') }}
                 </div>
 
                 <div class="links">
