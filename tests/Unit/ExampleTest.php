eade99047938ffc0b15098513a171f6870cfc406
liuhui <liuhui@github.com>
2019-08-16 22:21:23 +0800

    require swagger-php

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Support\Collection;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -15,5 +16,46 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+    }
+
+    public function testPerf()
+    {
+        $boxes = collect();
+
+        for ($i = 0; $i < 1000; $i++) {
+            $boxes->push(['box_id' => $i, 'box_no' => 'B' . $i]);
+        }
+        $this->generate($boxes);
+    }
+
+    protected function generate(Collection $boxes)
+    {
+        while (true) {
+            $remainingBoxes = $this->create($boxes);
+
+            if ($remainingBoxes->isEmpty()) {
+                return ;
+            }
+            $boxes = $remainingBoxes;
+        }
+    }
+
+    protected function create(Collection $boxes)
+    {
+        $remainingBoxes = collect();
+        while (true) {
+            $count = $boxes->count();
+            sleep(random_int(1,3));
+            if (!$count) {
+                return $remainingBoxes;
+            }
+
+            if ($count > 1) {
+                $remainingBoxes->push($boxes->pop());
+                continue;
+            }
+            break;
+        }
+        return $remainingBoxes;
     }
 }
